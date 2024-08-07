6896ab8fee0a983324fd949267eca0885512624a
Katlego <kdhlamini023b@student.wethinkcode.co.za>
2024-08-07 09:43:11 +0200

    creating server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('cors');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
